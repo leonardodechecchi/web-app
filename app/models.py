eec942dcfb94ae07eddb8eed875cbd4ef152d5fc
Leonardo <leo.dechi99@gmail.com>
2021-05-11 11:03:57 +0200

    not finished

--- app/models.py
+++ app/models.py
@@ -8,20 +8,28 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 
-class User(db.Model, UserMixin):
-    __tablename__ = "User"
-    __table_args__ = {'extend_existing': True}
-    social_number = db.Column(db.String(16), primary_key=True)
+class Person(db.Model):
+    __abstract__ = True
     name = db.Column(db.String(20), nullable=False)
     surname = db.Column(db.String(20), nullable=False)
+
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+
+class User(Person, UserMixin):
+    __tablename__ = 'User'
+    __table_args__ = {'extend_existing': True}
+    social_number = db.Column(db.String(16), primary_key=True)
     email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(60), nullable=False)
+    trainer_id = db.Column(db.Integer, db.ForeignKey('Trainer.id'))
 
     def __init__(self, social_number, name, surname, email, password):
-        self.surname = surname
-        self.email = email
-        self.name = name
+        super().__init__(name, surname)
         self.social_number = social_number
+        self.email = email
         self.password = password
 
     def __repr__(self):
@@ -30,3 +38,13 @@ class User(db.Model, UserMixin):
     # overriding get_id() : social_number it's our primary key 
     def get_id(self):
         return self.social_number
+
+
+class Trainer(Person):
+    __tablename__ = 'Trainer'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    users = db.relationship('User', backref='trainer', lazy=True)
+
+    def __repr__(self):
+        return f"Personal Trainer('{self.id}', '{self.name}', '{self.surname}')"
